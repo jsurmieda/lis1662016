f1ae97c84ed84688105ccfbf0e539c7f8a7b0cfd
Bryan Jerahmeel Yao <bgyao@up.edu.ph>
2016-05-18 23:37:53 +0900

    Articles Database Structure
    migration file and model for articles.
    NEEDED: migration files and models for Article Types, and Users.

--- app/Article.php
+++ app/Article.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
-    //
+    use SoftDeletes;
+	protected $dates = ['deleted_at'];
+
+	protected $fillable = [
+		'articletitle', 
+		'articlebody', 
+		'articletypes_id', 
+		'users_id',
+	];
+
+	public function articletypes() 
+	{
+			return $this->belongsto('App\Articletype');
+	}
+
+	public function users() 
+	{
+			return $this->hasMany('App\User');
+	}
 }
---
+++ database/migrations/2016_05_17_113309_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('articletitle');
+            $table->string('articlebody');//->unique();
+            $table->integer('articletypes_id');
+            $table->integer('users_id');
+            //$table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
