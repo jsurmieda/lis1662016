29dec639472c82dcc911b431021e57839f4db78d
embaluyut <embaluyut@up.edu.ph>
2016-05-27 15:43:18 +0800

    Emmanuel M. Baluyut

--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -5,7 +5,7 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Case Report</h3>
+                <h3>Case Reports</h3>
               </div>
 
             </div>
