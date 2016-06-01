4dceac7ec02d4becc6021f84bb127bb59146b2a5
Adrian Philip Luna <apbluna@gmail.com>
2016-06-01 14:23:11 +0800

    favicon
    added favicon line in adminframe

--- resources/views/layouts/adminframe.blade.php
+++ resources/views/layouts/adminframe.blade.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>NCIP Website</title>
+	  <!--favicon-->
+    <link rel="shortcut icon" href="{{{ asset('ncip.ico') }}}">
 
     <!-- Bootstrap -->
     <link href="{{ asset('vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
