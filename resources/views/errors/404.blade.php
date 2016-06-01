da704334bfed312d151bd5e289f9de304e5aed71
Adrian Philip Luna <apbluna@gmail.com>
2016-06-01 14:32:55 +0800

    404 redirect
    *updated 404 back button to redirect to previous page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,6 @@
     <section id="error" class="container text-center">
         <h1>404, Page not found</h1>
         <p>The Page you are looking for doesn't exist or an other error occurred.</p>
-        <a class="btn btn-primary" href="{{ url('/index') }}">GO BACK TO THE HOMEPAGE</a>
     </section><!--/#error-->
         <div class="container wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
             <div class="row">
