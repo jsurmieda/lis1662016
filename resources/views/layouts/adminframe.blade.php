ea2aa90aad3b125212e3c95a5c9e35ec586797c5
embaluyut <embaluyut@up.edu.ph>
2016-06-05 21:30:13 +0800

    Emmanuel M. Baluyut
    Aesthetics only

--- resources/views/layouts/adminframe.blade.php
+++ resources/views/layouts/adminframe.blade.php
@@ -9,7 +9,7 @@
 
     <title>NCIP Website</title>
 	  <!--favicon-->
-    <link rel="shortcut icon" href="{{{ asset('ncip.ico') }}}">
+    <link rel="shortcut icon" href="{{ asset('ncip.ico') }}">
 
     <!-- Bootstrap -->
     <link href="{{ asset('vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -26,7 +26,7 @@
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
   </head>
 
-  <body class="nav-md">
+  <body class="nav-md" onload="startTime()">
     <div class="container body">
       <div class="main_container">
         <div class="col-md-3 left_col">
@@ -273,82 +273,6 @@
           });
         </script>
 
-    <!-- Flot -->
-    <script>
-      $(document).ready(function() {
-        var data1 = [
-          [gd(2012, 1, 1), 17],
-          [gd(2012, 1, 2), 74],
-          [gd(2012, 1, 3), 6],
-          [gd(2012, 1, 4), 39],
-          [gd(2012, 1, 5), 20],
-          [gd(2012, 1, 6), 85],
-          [gd(2012, 1, 7), 7]
-        ];
-
-        var data2 = [
-          [gd(2012, 1, 1), 82],
-          [gd(2012, 1, 2), 23],
-          [gd(2012, 1, 3), 66],
-          [gd(2012, 1, 4), 9],
-          [gd(2012, 1, 5), 119],
-          [gd(2012, 1, 6), 6],
-          [gd(2012, 1, 7), 9]
-        ];
-        $("#canvas_dahs").length && $.plot($("#canvas_dahs"), [
-          data1, data2
-        ], {
-          series: {
-            lines: {
-              show: false,
-              fill: true
-            },
-            splines: {
-              show: true,
-              tension: 0.4,
-              lineWidth: 1,
-              fill: 0.4
-            },
-            points: {
-              radius: 0,
-              show: true
-            },
-            shadowSize: 2
-          },
-          grid: {
-            verticalLines: true,
-            hoverable: true,
-            clickable: true,
-            tickColor: "#d5d5d5",
-            borderWidth: 1,
-            color: '#fff'
-          },
-          colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
-          xaxis: {
-            tickColor: "rgba(51, 51, 51, 0.06)",
-            mode: "time",
-            tickSize: [1, "day"],
-            //tickLength: 10,
-            axisLabel: "Date",
-            axisLabelUseCanvas: true,
-            axisLabelFontSizePixels: 12,
-            axisLabelFontFamily: 'Verdana, Arial',
-            axisLabelPadding: 10
-          },
-          yaxis: {
-            ticks: 8,
-            tickColor: "rgba(51, 51, 51, 0.06)",
-          },
-          tooltip: false
-        });
-
-        function gd(year, month, day) {
-          return new Date(year, month - 1, day).getTime();
-        }
-      });
-    </script>
-    <!-- /Flot -->
-
     <!-- jVectorMap -->
     <script src="{{ asset('js/maps/jquery-jvectormap-world-mill-en.js') }}"></script>
     <script src="{{ asset('js/maps/jquery-jvectormap-us-aea-en.js') }}"></script>
@@ -435,5 +359,28 @@
     }
     </script>
     <!-- /add PersonsInvolved -->
+    <!-- /Time -->
+    <script>
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        var date = today.getDate();
+        var year = today.getFullYear();
+        ampm = today.getHours() >= 12 ? 'pm' : 'am';
+        months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('txt').innerHTML =
+        months[today.getMonth()] + ' ' + date + ', ' + year + ' ' + h + ":" + m + ":" + s + " " + ampm;
+        var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+    </script>
+    <!-- /time -->
   </body>
 </html>
