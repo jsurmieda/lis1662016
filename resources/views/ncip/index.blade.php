f915d884593310cf135ff3acac2cfb3ec28eb309
Bryan Jerahmeel Yao <bgyao@up.edu.ph>
2016-06-06 17:15:12 +0900

    Homepage Housekeeping
    - added comments for future reference.

--- resources/views/ncip/index.blade.php
+++ resources/views/ncip/index.blade.php
@@ -23,61 +23,21 @@
                                     <a class="btn-slide animation animated-item-3" href="{{ url('/article/'.$carousel->id ) }}">Read More</a>
                                 </div>
                             </div>
-<!-- yung poging lumad
+                        <!-- yung poging lumad
                             <div class="col-sm-6 hidden-xs animation animated-item-4">
                                 <div class="slider-img">
                                     <img src="images/slider/img1.png" class="img-responsive">
                                 </div>
-                            </div>
--->
+                            </div>-->
                         </div>
                     </div>
                 </div><!--/.item-->
         
-    <? php $counting++; ?>
-    @else:
-        break;
-    @endif
-@endforeach<!--
-                <div class="item" style="background-image: url(images/slider/bg2.jpg)">
-                    <div class="container">
-                        <div class="row slide-margin">
-                            <div class="col-sm-6">
-                                <div class="carousel-content">
-                                    <h1 class="animation animated-item-1">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-                                    <h2 class="animation animated-item-2">Accusantium doloremque laudantium totam rem aperiam, eaque ipsa...</h2>
-                                    <a class="btn-slide animation animated-item-3" href="#">Read More</a>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 hidden-xs animation animated-item-4">
-                                <div class="slider-img">
-                                    <img src="images/slider/img2.png" class="img-responsive">
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>/.item-->
-<!--
-                <div class="item" style="background-image: url(images/slider/bg3.jpg)">
-                    <div class="container">
-                        <div class="row slide-margin">
-                            <div class="col-sm-6">
-                                <div class="carousel-content">
-                                    <h1 class="animation animated-item-1">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-                                    <h2 class="animation animated-item-2">Accusantium doloremque laudantium totam rem aperiam, eaque ipsa...</h2>
-                                    <a class="btn-slide animation animated-item-3" href="#">Read More</a>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 hidden-xs animation animated-item-4">
-                                <div class="slider-img">
-                                    <img src="images/slider/img3.png" class="img-responsive">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>/.item-->
+                <? php $counting++; ?>
+        @else
+            break;
+        @endif<!-- ayaw gumana nito -->
+@endforeach
             </div><!--/.carousel-inner-->
         </div><!--/.carousel-->
         <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
